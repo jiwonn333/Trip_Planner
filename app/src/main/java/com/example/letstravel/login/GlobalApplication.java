bbfa6ae91fe1f49c221cc9c9dec4a5e4cc04f7a1
sinjiwon <sjwon113@naver.com>
2023-05-12 17:11:12 +0900

    chore: 카카오 SDK 초기화

---
+++ app/src/main/java/com/example/letstravel/login/GlobalApplication.java
@@ -0,0 +1,15 @@
+package com.example.letstravel.login;
+
+import android.app.Application;
+
+import com.example.letstravel.R;
+import com.kakao.sdk.common.KakaoSdk;
+
+public class GlobalApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        KakaoSdk.init(this, getString(R.string.kakao_app_key));
+    }
+}
