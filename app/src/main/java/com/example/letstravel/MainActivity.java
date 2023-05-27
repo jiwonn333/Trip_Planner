64e2313a520687b52b0a9148ec2d45c7e7ae65bd
sinjiwon <sjwon113@naver.com>
2023-05-27 21:33:48 +0900

    feat: bottom navigation 탐색 설정 및 서울 마커 찍기

---
+++ app/src/main/java/com/example/letstravel/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.letstravel;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map_container);
+        mapFragment.getMapAsync(this);
+
+        // bottom navigation 탐색 설정
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_container);
+        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav_view);
+        NavigationUI.setupWithNavController(bottomNav, navController);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng seoul = new LatLng(37.512017, 127.116902);
+        mMap.addMarker(new MarkerOptions().position(seoul).title("Marker in Seoul"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(seoul));
+    }
+}
