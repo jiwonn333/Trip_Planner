bf3eb6cebe9f203c23f638bd200f2eceb776f8ba
sinjiwon <sjwon113@naver.com>
2023-05-27 21:39:44 +0900

    design: google map 화면, activity 파일 추가

---
+++ app/src/main/java/com/example/letstravel/MapsActivity.java
@@ -0,0 +1,23 @@
+package com.example.letstravel;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+    }
+}
