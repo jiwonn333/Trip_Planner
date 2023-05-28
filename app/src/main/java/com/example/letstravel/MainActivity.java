07aa4339cc79e44bcd9cabf1d4f22f77eb000f6b
sinjiwon <sjwon113@naver.com>
2023-05-28 18:20:12 +0900

    refactor: 지도 위치 setDefaultLocation() 메서드로 변경

--- app/src/main/java/com/example/letstravel/MainActivity.java
+++ app/src/main/java/com/example/letstravel/MainActivity.java
@@ -1,23 +1,52 @@
 package com.example.letstravel;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationRequest;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
+    private MapView googlemap = null;
+    private Marker currentMarker = null;
+    private static final int GPS_ENABLE_REQUEST_CODE = 2001;
+    private static final int UPDATE_INTERVAL_MS = 1000; //1초
+    private final int FASTEST_UPDATE_INTERVAL_MS = 500; // 0.5초
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+    boolean needRequest = false;
+
+    // 앱을 실행하기 위해 필요한 퍼미션 정의
+    String[] REQUIRED_PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}; //외부저장소
+    private View mLayout;  // Snackbar 사용하기 위해 필요
+    private LocationRequest locationRequest;
+    private Location location;
+    private LocationCallback locationCallback;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +64,145 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         NavigationUI.setupWithNavController(bottomNav, navController);
     }
 
+
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
-        LatLng seoul = new LatLng(37.512017, 127.116902);
-        mMap.addMarker(new MarkerOptions().position(seoul).title("Marker in Seoul"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(seoul));
+
+        // 런타임 퍼미션 요청 대화상자나 GPS 활성 요청 대화상자 보이기전에 지도의 초기위치 서울로 지정
+        setDefaultLocation();
+
+//        // 앱에 런타임 퍼미션 요청
+//        getDeviceLocation();
+//
+//        //런타임 퍼미션 처리
+//        // 1. 위치 퍼미션을 가지고 있는지 체크합니다.
+//        int hasFineLocationPermission = ContextCompat.checkSelfPermission(this,
+//                Manifest.permission.ACCESS_FINE_LOCATION);
+//        int hasCoarseLocationPermission = ContextCompat.checkSelfPermission(this,
+//                Manifest.permission.ACCESS_COARSE_LOCATION);
+//
+//        if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
+//                hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED) {
+//
+//            // 이미 퍼미션을 가지고 있다면
+//            // ( 안드로이드 6.0 이하 버전은 런타임 퍼미션이 필요없기 때문에 이미 허용된 걸로 인식합니다.)
+//            // 위치 업데이트 시작
+//            startLocationUpdates();
+//        } else {
+//            // 퍼미션 요청을 허용한 적이 없다면 퍼미션 요청이 필요; 2가지 경우(3-1, 4-1)가 있습니다.
+//            // 사용자가 퍼미션 거부를 한 적이 있는 경우
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])) {
+//                // 요청을 진행하기 전에 사용자가에게 퍼미션이 필요한 이유를 설명해야 한다.
+//                Snackbar.make(mLayout, "이 앱을 실행하려면 위치 접근 권한이 필요합니다.", Snackbar.LENGTH_INDEFINITE).setAction("확인", view -> {
+//
+//                    // 사용자게에 퍼미션 요청. 요청 결과는 onRequestPermissionResult에서 수신
+//                    ActivityCompat.requestPermissions(MainActivity.this, REQUIRED_PERMISSIONS,
+//                            PERMISSIONS_REQUEST_CODE);
+//                }).show();
+//            } else {
+//                //  사용자가 퍼미션 거부를 한 적이 없는 경우에는 바로 퍼미션 요청
+//                // 요청 결과는 onRequestPermissionResult에서 수신
+//                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS,
+//                        PERMISSIONS_REQUEST_CODE);
+//            }
+//        }
+//
+//        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+//        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+//            @Override
+//            public void onMapClick(LatLng latLng) {
+//
+//                Log.d("test", "onMapClick :");
+//            }
+//        });
+
+
+        // 위치 권한 설정 여부에따라 구글맵에 디바이스의 위치를 설정
+//        updateLocationUI();
+
+//        LatLng seoul = new LatLng(37.512017, 127.116902);
+//        mMap.addMarker(new MarkerOptions().position(seoul).title("Marker in Seoul"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(seoul));
     }
+
+//    private void updateLocationUI() {
+//        if (mMap == null) {
+//            return;
+//        }
+//        /**
+//         * 기기의 위치를 알 수 있도록 위치 권한을 요청
+//         * 권한 요청의 결과는 onRequestPermissionsResult 콜백에 의해 처리
+//         */
+//        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+//                android.Manifest.permission.ACCESS_FINE_LOCATION)
+//                == PackageManager.PERMISSION_GRANTED) {
+//            mLocationPermissionGranted = true;
+//        } else {
+//            ActivityCompat.requestPermissions(this,
+//                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+//                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+//        }
+//        if (mLocationPermissionGranted) {
+//            mMap.setMyLocationEnabled(true);
+//            mMap.getUiSettings().setMyLocationButtonEnabled(true);
+//        } else {
+//            mMap.setMyLocationEnabled(false);
+//            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+//            mLastKnownLocation = null;
+//        }
+//    }
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+//                                           @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        mLocationPermissionGranted = false;
+//        switch (requestCode) {
+//            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+//                // If request is cancelled, the result arrays are empty.
+//                if (grantResults.length > 0
+//                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    mLocationPermissionGranted = true;
+//                }
+//            }
+//        }
+//        updateLocationUI();
+//    }
+
+
+    // 디바이스의 위치정보를 얻어서 맵 이동하기
+//    private void getDeviceLocation() {
+//        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+//                android.Manifest.permission.ACCESS_FINE_LOCATION)
+//                == PackageManager.PERMISSION_GRANTED) {
+//            mLocationPermissionGranted = true;
+//        } else {
+//            ActivityCompat.requestPermissions(this,
+//                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+//                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+//        }
+//    }
+
+    private void setDefaultLocation() {
+        //디폴트 위치; Seoul
+        LatLng DEFAULT_LOCATION_SEOUL = new LatLng(37.56, 126.97);
+        String markerTitle = "위치정보 가져올 수 없음";
+        String markerSnippet = "위치 퍼미션과 GPS 활성 요부 확인하세요";
+
+
+        if (currentMarker != null) currentMarker.remove();
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(DEFAULT_LOCATION_SEOUL);
+        markerOptions.title(markerTitle);
+        markerOptions.snippet(markerSnippet);
+        markerOptions.draggable(true);
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+        currentMarker = mMap.addMarker(markerOptions);
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(DEFAULT_LOCATION_SEOUL, 15);
+        mMap.moveCamera(cameraUpdate);
+    }
+
 }
