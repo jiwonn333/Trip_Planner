409cb524cf339974d888a3568447e960a15dd828
sinjiwon <sjwon113@naver.com>
2023-06-09 15:00:37 +0900

    refactor: InformationFragment, SearchFragment 에서 initView(), initObserver() 메서드로 리팩토링

--- app/src/main/java/com/example/letstravel/fragment/information/InformationFragment.java
+++ app/src/main/java/com/example/letstravel/fragment/information/InformationFragment.java
@@ -18,40 +18,39 @@ import com.example.letstravel.fragment.search.SearchFragment;
 public class InformationFragment extends Fragment {
 
     private FragmentInformationBinding binding;
+    private InformationViewModel informationViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        InformationViewModel informationViewModel =
-                new ViewModelProvider(this).get(InformationViewModel.class);
-
         binding = FragmentInformationBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-
-//        final Toolbar toolbar = binding.infoToolbar.toolbar;
-//        informationViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        initView();
+        initObserver();
 
-        // 검색창 클릭하면 SearchFragment로 이동
+
+    }
+
+    private void initView() {
         binding.toolbar.setOnClickListener(v -> {
-//            SearchFragment searchFragment = new SearchFragment();
-//            FragmentTransaction fragmentTransaction = getParentFragmentManager().beginTransaction();
-//
-//            fragmentTransaction.replace(R.id.frameLayout, searchFragment);
-//            fragmentTransaction.addToBackStack(null);
-//            fragmentTransaction.commit();
-
             FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
             SearchFragment searchFragment = new SearchFragment();
-            // HomeFragment 로 transaction
             fragmentTransaction.replace(R.id.frameLayout, searchFragment).commit();
         });
+    }
 
+    private void initObserver() {
+        informationViewModel = new ViewModelProvider(requireActivity()).get(InformationViewModel.class);
+        informationViewModel.getSearchWord().observe(getViewLifecycleOwner(), search -> {
+//            informationViewModel.getSearchWord();
+            binding.tvSearch.setText(search);
+        });
     }
 
     @Override
--- app/src/main/java/com/example/letstravel/fragment/search/SearchFragment.java
+++ app/src/main/java/com/example/letstravel/fragment/search/SearchFragment.java
@@ -1,10 +1,13 @@
 package com.example.letstravel.fragment.search;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,45 +15,44 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.letstravel.R;
 import com.example.letstravel.databinding.FragmentSearchBinding;
+import com.example.letstravel.fragment.information.InformationViewModel;
 
 public class SearchFragment extends Fragment {
     private FragmentSearchBinding binding;
-
-    private SearchViewModel mViewModel;
-
-    public static SearchFragment newInstance() {
-        return new SearchFragment();
-    }
+    private InformationViewModel informationViewModel;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        SearchViewModel searchViewModel =
-                new ViewModelProvider(this).get(SearchViewModel.class);
+        informationViewModel = new ViewModelProvider(requireActivity()).get(InformationViewModel.class);
 
         binding = FragmentSearchBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-        binding.ibBack.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("CommitTransaction")
-            @Override
-            public void onClick(View v) {
-                FragmentTransaction fragmentTransaction = getParentFragmentManager().beginTransaction().addToBackStack(null);
-                fragmentTransaction.remove(SearchFragment.this);
-                fragmentTransaction.commit();
-
-            }
-        });
-
         return root;
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(SearchViewModel.class);
-        // TODO: Use the ViewModel
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        initView();
     }
 
+    private void initView() {
+        binding.ibBack.setOnClickListener(v -> {
+            FragmentTransaction fragmentTransaction = getParentFragmentManager().beginTransaction().addToBackStack(null);
+            fragmentTransaction.remove(SearchFragment.this).commit();
+        });
+
+        binding.etSearch.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                Toast.makeText(getContext(), binding.etSearch.getText().toString() + " 검색", Toast.LENGTH_SHORT).show();
+                informationViewModel.setSearchWord(binding.etSearch.getText().toString());
+                FragmentTransaction fragmentTransaction = getParentFragmentManager().beginTransaction().addToBackStack(null);
+                fragmentTransaction.remove(SearchFragment.this).commit();
+
+            }
+            return false;
+        });
+    }
 }
