101c5c9c155d9f2b2d001785e61f030333c597de
shinjiwon <shinjiwon3378@gmail.com>
2023-11-05 20:04:24 +0900

    test: Search TextView 클릭 시 api 통신 화면으로 이동

--- app/src/main/java/com/example/letstravel/fragment/test/TestFragment1.kt
+++ app/src/main/java/com/example/letstravel/fragment/test/TestFragment1.kt
@@ -1,29 +1,33 @@
 package com.example.letstravel.fragment.test
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.letstravel.R
+import androidx.lifecycle.ViewModelProvider
+import com.example.letstravel.databinding.FragmentTestFragment1Binding
 import com.example.letstravel.fragment.common.BaseFragment
 
 class TestFragment1 : BaseFragment() {
 
     private lateinit var viewModel: TestFragment1ViewModel
-
+    private var binding: FragmentTestFragment1Binding? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_test_fragment1, container, false)
+        binding = FragmentTestFragment1Binding.inflate(inflater, container, false)
+        return binding!!.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel = ViewModelProvider(this).get(TestFragment1ViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        // textview 클릭시 검색할 수 잇는 창으로 이동
+        binding?.tvSearch?.setOnClickListener {
+            replaceFragment(this@TestFragment1, TestFragment1Directions.actionNavigationTestToSearchTest())
+        }
     }
 }
